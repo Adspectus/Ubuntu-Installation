b257dcaed2a9aeff99590c00fb4ae6cf17d19626
Uwe Gehring <uwe@imap.cc>
2025-01-19 13:07:09 +0100

    Added option vga

--- createQEMU.sh
+++ createQEMU.sh
@@ -4,10 +4,8 @@ qemu-img create -f raw ubuntu-24.04.1-custom-desktop-amd64.img 100G
 
 qemu-system-x86_64 -daemonize -enable-kvm \
   -name "Custom Ubuntu 24.04" \
-  -cpu host -smp 4 -m 16384 \
+  -cpu host -smp 4 -m 16384 -vga virtio \
   -drive file=ubuntu-24.04.1-custom-desktop-amd64.img,format=raw \
   -cdrom ubuntu-24.04.1-custom-desktop-amd64.iso -boot d
 
 exit 0
-
-#  -device virtio-vga-gl -display sdl,gl=on \
--- runQEMU.sh
+++ runQEMU.sh
@@ -2,7 +2,7 @@
 
 qemu-system-x86_64 -daemonize -enable-kvm \
   -name "Custom Ubuntu 24.04" \
-  -cpu host -smp 4 -m 16384 \
+  -cpu host -smp 4 -m 16384 -vga virtio \
   -drive file=ubuntu-24.04.1-custom-desktop-amd64.img,format=raw \
   -nic user,hostfwd=::2222-:22
 
